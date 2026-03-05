dfaac210b64f2ac3547fcdc572f8cc1611c7e9b6
Dodi Aditya <dodiaridharma@gmail.com>
2026-03-05 14:46:52 +0800

    refactor: using macro-based definitions for emits and props rules

--- packages/eslint-plugin-nuxt/src/rules/no-direct-any-in-emits.ts
+++ packages/eslint-plugin-nuxt/src/rules/no-direct-any-in-emits.ts
@@ -1,68 +1,9 @@
-import { AST_NODE_TYPES } from "@typescript-eslint/utils";
-import { createRule, withTemplateVisitor } from "../utils/rule";
-import { isVueFile } from "../utils/filename";
-import { checkCallExpressionName } from "../utils/ast";
+import { createNoDirectAnyRule } from "../utils/define-macro-rule";
 
-export const noDirectAnyInEmits = createRule({
-  name: "no-direct-any-in-emits",
-  meta: {
-    docs: {
-      description: "Disallow 'any' type in component emits definition.",
-    },
-    type: "problem",
-    messages: {
-      "issue:any-in-emits":
-        "Avoid using 'any' type in emits. Use specific types instead.",
-    },
-    schema: [
-      {
-        type: "object",
-        properties: {
-          allowUnknown: {
-            type: "boolean",
-            description: "Allow 'unknown' type in emits. Default is false.",
-          },
-        },
-        additionalProperties: false,
-      },
-    ],
-    defaultOptions: [{ allowUnknown: false }],
-    hasSuggestions: false,
-  },
-  create: (context) => {
-    if (!isVueFile(context.filename)) return {};
-
-    const allowUnknown = context.options[0]?.allowUnknown ?? false;
-
-    return withTemplateVisitor(context, {
-      script: {
-        CallExpression(node) {
-          if (checkCallExpressionName(node, "defineEmits")) {
-            const hasTypeParameter = !!(
-              node.typeArguments && node.typeArguments.params.length > 0
-            );
-
-            if (!hasTypeParameter) {
-              return;
-            }
-
-            const disallowedTypes = [AST_NODE_TYPES.TSAnyKeyword];
-            if (!allowUnknown) {
-              disallowedTypes.push(AST_NODE_TYPES.TSUnknownKeyword);
-            }
-
-            if (
-              node.typeArguments &&
-              disallowedTypes.includes(node.typeArguments?.params[0].type)
-            ) {
-              context.report({
-                node,
-                messageId: "issue:any-in-emits",
-              });
-            }
-          }
-        },
-      },
-    });
-  },
+export const noDirectAnyInEmits = createNoDirectAnyRule({
+  macroName: "defineEmits",
+  ruleName: "no-direct-any-in-emits",
+  description: "Disallow 'any' type in component emits definition.",
+  messageId: "issue:any-in-emits",
+  messageText: "Avoid using 'any' type in emits. Use specific types instead.",
 });
