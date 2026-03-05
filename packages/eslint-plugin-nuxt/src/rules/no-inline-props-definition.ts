dcdf14cf4c0e5d24467ebc3ab78dbed96e9efe00
Dodi Aditya <dodiaridharma@gmail.com>
2026-03-05 14:46:52 +0800

    refactor: using macro-based definitions for emits and props rules

--- packages/eslint-plugin-nuxt/src/rules/no-inline-props-definition.ts
+++ packages/eslint-plugin-nuxt/src/rules/no-inline-props-definition.ts
@@ -1,79 +1,16 @@
-import { AST_NODE_TYPES } from "@typescript-eslint/utils";
-import { checkCallExpressionName } from "../utils/ast";
-import { isVueFile } from "../utils/filename";
-import { createRule, withTemplateVisitor } from "../utils/rule";
+import { createNoInlineDefinitionRule } from "../utils/define-macro-rule";
 
-export const noInlinePropsDefinition = createRule({
-  name: "no-inline-props-definition",
-  meta: {
-    docs: {
-      description:
-        "Disallow inline props definition if property count exceeds limit.",
-    },
-    type: "suggestion",
-    messages: {
-      "issue:empty-inline-object":
-        "Avoid using an empty object for props definition. Use type-based declaration instead.",
-      "issue:inline-object":
-        "Avoid using inline object with greater than {{limit}} properties for props definition. Use type-based declaration instead.",
-    },
-    schema: {
-      type: "array",
-      items: [
-        {
-          type: "object",
-          properties: {
-            propertiesLimit: { type: "number" },
-          },
-        },
-      ],
-    },
-    defaultOptions: [{ propertiesLimit: 2 }],
-    hasSuggestions: false,
-  },
-  create: (context) => {
-    if (!isVueFile(context.filename)) return {};
-
-    return withTemplateVisitor(context, {
-      script: {
-        CallExpression(node) {
-          if (checkCallExpressionName(node, "defineProps")) {
-            const hasTypeParameter = !!(
-              node.typeArguments && node.typeArguments.params.length > 0
-            );
-
-            if (!hasTypeParameter) {
-              return;
-            }
-
-            if (
-              node.typeArguments?.params[0].type ===
-              AST_NODE_TYPES.TSTypeLiteral
-            ) {
-              const memberCount = node.typeArguments.params[0].members.length;
-
-              if (memberCount === 0) {
-                context.report({
-                  node,
-                  messageId: "issue:empty-inline-object",
-                });
-
-                return;
-              }
-
-              if (memberCount > context.options[0].propertiesLimit) {
-                context.report({
-                  node,
-                  messageId: "issue:inline-object",
-                  data: {
-                    limit: context.options[0].propertiesLimit,
-                  },
-                });
-              }
-            }
-          }
-        },
-      },
-    });
-  },
+export const noInlinePropsDefinition = createNoInlineDefinitionRule({
+  macroName: "defineProps",
+  ruleName: "no-inline-props-definition",
+  description:
+    "Disallow inline props definition if property count exceeds limit.",
+  limitOptionKey: "propertiesLimit",
+  defaultLimit: 2,
+  emptyMessageId: "issue:empty-inline-object",
+  emptyMessageText:
+    "Avoid using an empty object for props definition. Use type-based declaration instead.",
+  inlineMessageId: "issue:inline-object",
+  inlineMessageText:
+    "Avoid using inline object with greater than {{limit}} properties for props definition. Use type-based declaration instead.",
 });
