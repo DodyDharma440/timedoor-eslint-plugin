ac75ba3e50c3c08e71b82d1d7020637a6b83c8d2
Dodi Aditya <dodiaridharma@gmail.com>
2026-03-30 16:18:48 +0800

    feat: implement ESLint configuration package and add standard rules (flat config)

--- examples/classic/app.vue
+++ examples/classic/app.vue
@@ -3,12 +3,24 @@ interface AppProps {
   message: string;
 }
 
+const someFunc = (n1: number, n2: number) => {
+  const result = n1 + n2;
+
+  return result;
+};
+
+console.log("asdasad");
+
 interface AppEmits {
   submit: (value: string) => void;
 }
 
-defineProps<AppProps>();
+defineProps<{ hello: string }>();
 defineEmits<AppEmits>();
+
+if (import.meta.client) {
+  useAsyncData("data", () => $fetch("/api/data"));
+}
 </script>
 
 <template>
--- packages/eslint-config-nuxt/src/index.ts
+++ packages/eslint-config-nuxt/src/index.ts
@@ -1,13 +1,29 @@
 import { ESLint } from "eslint";
+import { standardRules } from "./rules/standard";
 
 const eslintConfig: ESLint.ConfigData = {
-  plugins: ["@timedoor/eslint-plugin-nuxt"],
-  extends: ["plugin:@timedoor/eslint-plugin-nuxt/recommended"],
+  plugins: [
+    "@timedoor/eslint-plugin-nuxt",
+    "vue",
+    "@typescript-eslint",
+    "regexp",
+  ],
+  extends: [
+    "plugin:@timedoor/eslint-plugin-nuxt/recommended",
+    "plugin:vue/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:promise/recommended",
+    "plugin:sonarjs/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:case-police/recommended",
+  ],
   overrides: [
     {
       files: ["*.ts"],
     },
   ],
+  rules: { ...standardRules },
 };
 
 export = eslintConfig;
