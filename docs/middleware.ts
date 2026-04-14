802213655af62f39aeaf07dcd07f237ee4015f3d
Dodi Aditya <dodiaridharma@gmail.com>
2026-04-14 10:12:43 +0800

    feat: basic auth user password from env

--- docs/middleware.ts
+++ docs/middleware.ts
@@ -11,7 +11,10 @@ function middleware(req: Request) {
     const authValue = basicAuth.split(" ")[1];
     const [user, pwd] = atob(authValue).split(":");
 
-    if (user === "4dmin" && pwd === "testpwd123") {
+    if (
+      user === process.env.BASIC_AUTH_USER &&
+      pwd === process.env.BASIC_AUTH_PASSWORD
+    ) {
       return next();
     }
   }
